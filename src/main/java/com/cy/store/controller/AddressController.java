f16f519bb1cae449646518d806199448ba71ade8
tzz2015 <1374329135@qq.com>
2022-08-06 15:17:39 +0800

    地址列表

--- src/main/java/com/cy/store/controller/AddressController.java
+++ src/main/java/com/cy/store/controller/AddressController.java
@@ -4,6 +4,7 @@ import com.cy.store.entity.Address;
 import com.cy.store.service.IAddressService;
 import com.cy.store.util.JsonResult;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,4 +35,21 @@ public class AddressController extends BaseController {
         Integer uid = getUidFromSession(httpSession);
         return new JsonResult<>(OK, addressService.findByUid(uid));
     }
+
+    @RequestMapping("{aid}/set_default")
+    public JsonResult<Void> setDefault(@PathVariable("aid") Integer aid, HttpSession session) {
+        Integer uid = getUidFromSession(session);
+        String username = getUsernameFromSession(session);
+        addressService.setDefault(aid, uid, username);
+        return new JsonResult<Void>(OK);
+    }
+
+    @RequestMapping("{aid}/delete")
+    public JsonResult<Void> delete(@PathVariable("aid") Integer aid, HttpSession session) {
+        Integer uid = getUidFromSession(session);
+        String username = getUsernameFromSession(session);
+        addressService.delete(aid, uid, username);
+        return new JsonResult<Void>(OK);
+    }
+
 }
--- src/main/java/com/cy/store/service/IAddressService.java
+++ src/main/java/com/cy/store/service/IAddressService.java
@@ -13,4 +13,28 @@ public interface IAddressService {
     void addNewAddress(Integer uid, String username, Address address);
 
     List<Address> findByUid(Integer uid);
+
+    /**
+     * 设置默认收货地址
+     * @param aid 收货地址id
+     * @param uid 归属的用户id
+     * @param username 当前登录的用户名
+     */
+    void setDefault(Integer aid, Integer uid, String username);
+
+    /**
+     * 删除收货地址
+     * @param aid 收货地址id
+     * @param uid 归属的用户id
+     * @param username 当前登录的用户名
+     */
+    void delete(Integer aid, Integer uid, String username);
+
+    /**
+     * 根据收货地址数据的id，查询收货地址详情
+     * @param aid 收货地址id
+     * @param uid 归属的用户id
+     * @return 匹配的收货地址详情
+     */
+    Address getByAid(Integer aid, Integer uid);
 }
